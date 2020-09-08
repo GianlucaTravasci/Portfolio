b2dd9dcd8c1a951920b696fd889174c34c18ad46
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-08 17:59:26 +0200

    Updates

---
+++ precache-manifest.0879776be5fc78dda4397afad5cbfeb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308f9f3e80f22f78404bc52877133455",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/Portfolio/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827d5b67bdc062e07597",
+    "url": "/Portfolio/static/js/main.cfed5a46.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  }
+]);
